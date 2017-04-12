6405033bafdea42789921b76e6ef005ae27f9c3b
lulizhu <15810307170@163.com>
2017-04-12 15:49:42 +0800

    first commit

--- app/src/main/java/net/m56/ckkj/watermeters/ui/BaseActivity.java
+++ app/src/main/java/net/m56/ckkj/watermeters/ui/BaseActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
@@ -33,6 +34,7 @@ public abstract class BaseActivity extends Activity  implements  DialogMaker.Dia
         DisplayMetrics dm = getResources().getDisplayMetrics();
 
         screenW = dm.widthPixels;
+        Log.e("screenWidth==" , String.valueOf(screenW));
         screenH = dm.heightPixels;
     }
 
